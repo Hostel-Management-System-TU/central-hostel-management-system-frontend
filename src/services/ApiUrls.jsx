8d37f92bf0cd571b4a6152e6e1f75bffa9580949
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2026-05-21 19:22:04 +0530

    fix: base urls

--- src/services/ApiUrls.jsx
+++ src/services/ApiUrls.jsx
@@ -1,7 +1,6 @@
-export const baseApi = "http://127.0.0.1:7860/api/v1"
-// export const baseApi = "https://chms78-chms-api.hf.space/api/v1"
+export const baseApi = import.meta.env.VITE_BASE_URL || "http://localhost:7860"
+export const R2DevURL = import.meta.env.VITE_R2_URL || ""
 
-export const R2DevURL = "https://pub-f2cb6691fe21423a9c423ad220580d86.r2.dev/"
 
 export const urls = {
     reference: {
